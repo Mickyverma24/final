68ec3e4434ed0ad8625618e8211274fb631be825
Ravi verma <vermaravi8809@gmail.com>
2024-05-14 14:33:02 +0530

    retrive endpoint done

---
+++ backend/controllers/quizControllers/retriveAllStudentsforRespectiveQuiz.js
@@ -0,0 +1,23 @@
+import { Student } from "../../models/student.model.js";
+
+const retrieveAllStudents = async (req, res) => {
+  try {
+    // Extract the user from the request object
+    const { quizId } = req.body;
+    if (!quizId) {
+      return res.status(400).send({ message: "Error: NO Id found of Quiz.." });
+    }
+
+    // Check if user exists
+    const studentsOfRespectiveQuiz = await Student.find({
+      respectiveQuizId: quizId,
+    });
+
+    // Return the found quizzes
+    return res.status(200).send(studentsOfRespectiveQuiz);
+  } catch (error) {
+    console.error("Error while retrieving students of quizz:", error);
+    return res.status(500).send({ message: "Internal Server Error." });
+  }
+};
+export default retrieveAllStudents;
--- backend/routes/quiz.routes.js
+++ backend/routes/quiz.routes.js
@@ -3,11 +3,13 @@ import { newQuiz } from "../controllers/quizControllers/newQuiz.js";
 import { protectedUser } from "../middlewares/protectedUser.js";
 import { evaluatedUser } from "../controllers/quizControllers/evaluatedUser.js";
 import  retriveAllQuiz  from "../controllers/quizControllers/retriveAllQuizforRespectiveUser.js";
+import retrieveAllStudents from "../controllers/quizControllers/retriveAllStudentsforRespectiveQuiz.js";
 
 const router = express.Router();
 
 router.post("/newquiz",protectedUser,newQuiz);
 router.post("/student/:id",protectedUser,evaluatedUser)
 router.get("/retriveAllQuiz",protectedUser,retriveAllQuiz);
+router.get("/retriveAllStudent",protectedUser,retrieveAllStudents);
 
 export default router;
